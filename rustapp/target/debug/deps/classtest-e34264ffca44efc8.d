370ad3e7a6ad021e7d5c991463bb256a518697cd
anashr18 <anand.shri18@gmail.com>
2025-03-18 03:39:44 +0000

    .gitignore

--- rustapp/target/debug/deps/classtest-e34264ffca44efc8.d
+++
@@ -1,5 +0,0 @@
-/home/ubuntu/TS_Rust/rustapp/target/debug/deps/libclasstest-e34264ffca44efc8.rmeta: src/bin/classes/class.rs
-
-/home/ubuntu/TS_Rust/rustapp/target/debug/deps/classtest-e34264ffca44efc8.d: src/bin/classes/class.rs
-
-src/bin/classes/class.rs:
